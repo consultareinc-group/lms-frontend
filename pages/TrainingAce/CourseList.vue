d771c2445f53eb34bef85022ef478464ccfa0061
Jerald Bianan <jeraldbianan@gmail.com>
2025-03-07 04:14:46 +0800

    updated the getCourses function in CourseList component

--- pages/TrainingAce/CourseList.vue
+++ pages/TrainingAce/CourseList.vue
@@ -94,15 +94,13 @@ const getCourses = () => {
     .then((response) => {
       if (response.status === "success") {
         response.data.forEach((data) => {
-          if (data.status === 1) {
-            courseStore.GetCourse({ id: data.id }).then((response) => {
-              const courseDetails = {
-                ...response.data,
-              };
+          courseStore.GetCourse({ id: data.id }).then((response) => {
+            const courseDetails = {
+              ...response.data,
+            };
 
-              courses.value.push(courseDetails);
-            });
-          }
+            courses.value.push(courseDetails);
+          });
         });
       }
     })

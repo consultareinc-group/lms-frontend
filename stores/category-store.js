40fa8715ed8adaac097dd97a4122b29507a24e0f
Jerald Bianan <jeraldbianan@gmail.com>
2025-03-13 14:21:47 +0800

    added GetCategory and EditCategory API endpoints in category-store

--- stores/category-store.js
+++ stores/category-store.js
@@ -16,5 +16,29 @@ export const useCategoryStore = defineStore("category", {
           });
       });
     },
+    GetCategory(request) {
+      return new Promise((resolve, reject) => {
+        api
+          .get(`lms/category/${request.id}`)
+          .then((response) => {
+            resolve(response.data);
+          })
+          .catch((error) => {
+            reject(error.response.data);
+          });
+      });
+    },
+    EditCategory(request) {
+      return new Promise((resolve, reject) => {
+        api
+          .put(`lms/category/${request.id}`, request.payload)
+          .then((response) => {
+            resolve(response.data);
+          })
+          .catch((error) => {
+            reject(error.response.data);
+          });
+      });
+    },
   },
 });

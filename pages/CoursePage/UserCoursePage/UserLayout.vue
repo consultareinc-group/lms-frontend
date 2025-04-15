74c545cd44e9b678ee59de5c9009de341ecd3468
Jerald Bianan <jeraldbianan@gmail.com>
2025-04-16 01:02:12 +0800

    refactor, added Login functionality and User information

--- pages/CoursePage/UserCoursePage/UserLayout.vue
+++ pages/CoursePage/UserCoursePage/UserLayout.vue
@@ -19,17 +19,64 @@
           </a>
         </q-toolbar-title>
 
-        <div class="q-ml-auto q-px-xl full-height flex items-center">
+        <div
+          v-if="
+            authStore.UserInformation &&
+            Object.keys(authStore.UserInformation).length > 0
+          "
+          class="q-ml-auto q-px-xl full-height flex items-center"
+        >
           <q-btn
-            label="Browse More Courses"
+            v-if="authStore.UserInformation.role === 0"
+            label="Course Management Dashboard"
+            no-caps
+            rounded
+            class="bg-accent text-white q-px-xl q-py-sm q-mr-sm"
+            :to="{ name: 'Course Management' }"
+          />
+
+          <q-btn
+            label="View All Courses"
             no-caps
             rounded
             class="bg-accent text-white q-px-xl q-py-sm"
             @click="browseMoreCourses"
           />
+
+          <div class="q-ml-auto q-px-md full-height">
+            <q-btn-dropdown flat :ripple="false" no-caps class="full-height">
+              <template v-slot:label>
+                <q-avatar>
+                  <img src="https://cdn.quasar.dev/img/avatar.png" />
+                </q-avatar>
+                <span class="q-ml-md text-grey-9"
+                  >{{ authStore.UserInformation.first_name }},
+                  {{ authStore.UserInformation.last_name }}</span
+                >
+              </template>
+              <q-list>
+                <q-item clickable v-close-popup @click="logout">
+                  <q-item-section>
+                    <q-item-label>Sign out</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-btn-dropdown>
+          </div>
+        </div>
+        <div v-else class="q-ml-auto q-px-md full-height flex items-center">
+          <q-btn
+            label="Login"
+            no-caps
+            rounded
+            class="bg-accent text-white q-px-xl q-py-sm"
+            @click="showLoginDialog = true"
+          />
         </div>
       </q-toolbar>
     </q-header>
+
+    <LoginDialog v-model="showLoginDialog" @close="closeDialog"></LoginDialog>
 
     <q-page-container class="">
       <router-view />
@@ -47,27 +94,34 @@
 
 <script setup>
 import { ref } from "vue";
+import { px } from "src/lib/utils";
+import { useRoute, useRouter } from "vue-router";
+import { useQuasar } from "quasar";
+
+import { useAuthStore } from "src/stores/auth";
+
 import { HEADER_HEIGHT, SIDEBAR_WIDTH } from "src/lib/vars";
 import BrandLogo from "src/components/BrandLogo.vue";
+
+import LoginDialog from "../../TrainingAce/components/LoginDialog.vue";
 
 // Variables
 const route = useRoute();
 const router = useRouter();
 const $q = useQuasar();
-const leftDrawerOpen = ref(false);
+const authStore = useAuthStore();
+// const leftDrawerOpen = ref(false);
 
-import { px } from "src/lib/utils";
-import { useRoute, useRouter } from "vue-router";
-import { useQuasar } from "quasar";
+const showLoginDialog = ref(false);
 
 defineOptions({
   name: "UserLayout",
 });
 
 // Functions
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
-}
+// function toggleLeftDrawer() {
+//   leftDrawerOpen.value = !leftDrawerOpen.value;
+// }
 
 const browseMoreCourses = () => {
   if (route.name === "Courses") {
@@ -84,5 +138,25 @@ const browseMoreCourses = () => {
     name: "Courses",
     params: { category_id: " ", category_name: "All" },
   });
+};
+
+const logout = () => {
+  authStore.LogoutUser().finally(() => {
+    router.push({ name: "Select Category" });
+  });
+
+  authStore.UserInformation = {};
+
+  $q.notify({
+    message: `<p class='q-mb-none'>You have successfully logged out.</p>`,
+    color: "green-2",
+    position: "top",
+    textColor: "green",
+    html: true,
+  });
+};
+
+const closeDialog = () => {
+  showLoginDialog.value = false;
 };
 </script>
